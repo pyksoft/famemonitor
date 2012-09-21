ab0f6f1b951f20b1efcbd040d5096bf6a4dd1604
pyksoft <pyksoft@gmail.com>
2012-09-21 14:57:29 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Famemonitor::Application.config.secret_token = 'b849f60b8b327652c6e9636e12e9322b564497d7c0343cd8757381d680ac9aa34e8309d33aa06b7f0b1bb050fafae8e4c66788a8706a47d25a87d38eb35e70fb'
