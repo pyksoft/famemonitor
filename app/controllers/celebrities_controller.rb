ae58c13796eaa11b0fb89039f6866a8429a0f67a
pyksoft <pyksoft@gmail.com>
2013-01-08 21:45:43 +0530

    Fixed home page layout

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -1,23 +1,17 @@
 class CelebritiesController < ApplicationController
   # GET /celebrities
   # GET /celebrities.json
+
   def index
     @celebrities = Celebrity.paginate(:page => params[:page], :per_page => 30)    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @celebrities }
-    end
+    render layout: 'home'
   end
 
   # GET /celebrities/1
   # GET /celebrities/1.json
   def show
     @celebrity = Celebrity.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @celebrity }
-    end
+    render layout: 'home'
   end
 
   # GET /celebrities/new
